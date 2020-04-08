f2eba1c8d2771005d2f999c1925fdcd4a3231997
Grzegorz Holak <grzegorz.holak@amberteam.pl>
2020-04-08 17:58:59 +0200

    wydzielenie basetest przed webinarem

--- src/test/kotlin/pl/amberteam/BaseTest.kt
+++ src/test/kotlin/pl/amberteam/BaseTest.kt
@@ -1,21 +1,19 @@
 package pl.amberteam
 
-import io.restassured.RestAssured
+import io.restassured.RestAssured.*
 import org.junit.jupiter.api.BeforeAll
 
 
 open class BaseTest {
-
-
     companion object {
-        protected val apiVersion = "1"
+        protected const val API_VERSION = "1"
 
         @BeforeAll
         @JvmStatic
         fun setup() {
-            RestAssured.baseURI = "http://localhost"
-            RestAssured.basePath = "/api/rest/v${apiVersion}/"
-            RestAssured.port = 9999
+            baseURI = "http://localhost"
+            basePath = "/api/rest/v${API_VERSION}/"
+            port = 9999
 
         }
     }
